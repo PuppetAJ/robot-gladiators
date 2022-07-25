40d7e6a423b8aa422be32b1f15db5bfe7084a330
Egg <adrianjimenez1950@gmail.com>
2022-07-24 22:41:22 -0400

    added robot names array, and functionality to fight multiple robots per round.

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,16 +6,16 @@ var playerAttack = 10;
 
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roberto";
+
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 
 var enemyHealth = 50;
 
 var enemyAttack = 12;
 
 // create function (expression)
-var fight = function () {
+var fight = function (enemyName) {
     window.alert("Welcome to Robot Gladiators");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     // if player choses to fight, then fight
@@ -66,4 +66,8 @@ var fight = function () {
 };
 
 // execute function (expression)
-fight();
+// fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
