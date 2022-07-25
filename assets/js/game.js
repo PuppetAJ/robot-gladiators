97d0bcc15696860f20e0f3c4f57051d727d92d3c
Egg <adrianjimenez1950@gmail.com>
2022-07-24 23:25:03 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,10 @@
 var playerName = window.prompt("What is your robot's name?");
-window.alert("Welcome to Robot Gladiators");
 
 var playerHealth = 100;
 
 var playerAttack = 10;
 
 var playerMoney = 10;
-
 
 
 var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
@@ -68,8 +66,18 @@ var fight = function(enemyName) {
 // fight();
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
+
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
     
 }
