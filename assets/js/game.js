c75b41255a4e66e0a79a359d4d72d84fb5299eb5
Egg <adrianjimenez1950@gmail.com>
2022-07-27 22:27:45 -0400

    shop now accepts numerical values

--- assets/js/game.js
+++ assets/js/game.js
@@ -200,22 +200,19 @@ var endGame = function() {
 
 var shop = function() {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE to make a choice."
     )
 
-    switch (shopOptionPrompt) {
-        case "REFILL": // since there is no code here it falls through to the code below without a "break;"
-        case "refill":
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
+    switch (shopOptionPrompt) { // since there is no code here it falls through to the code below without a "break;"
+        case 1:
             playerInfo.refillHealth();
             break;
-        
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             break;
         default:
